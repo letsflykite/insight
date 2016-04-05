cb1f8e9fcd74232aaf064604c29090f6d739041d
letsflykite <joannehsun@gmail.com>
2016-04-04 21:31:42 -0700

    change to unix format

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
 #python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/output.txt
-javac -cp ".;./src/jackson-databind-2.7.3.jar;./src/jackson-core-2.7.3.jar;./src/jackson-annotations-2.7.3.jar" *.java
-
-java -cp "./;./src;./src/jackson-databind-2.7.3.jar;./src/jackson-core-2.7.3.jar;./src/jackson-annotations-2.7.3.jar" AverageDegree ./tweet_input/tweets.txt ./tweet_output/output.txt
-
+#javac -cp ".;./src/jackson-databind-2.7.3.jar;./src/jackson-core-2.7.3.jar;./src/jackson-annotations-2.7.3.jar" src/AverageDegree.java
+javac -cp ".:./src/jackson-databind-2.7.3.jar:./src/jackson-core-2.7.3.jar:./src/jackson-annotations-2.7.3.jar" src/AverageDegree.java
+#java -cp "./;./src;./src/jackson-databind-2.7.3.jar;./src/jackson-core-2.7.3.jar;./src/jackson-annotations-2.7.3.jar" AverageDegree ./tweet_input/tweets.txt ./tweet_output/output.txt
+java -cp "./:./src:./src/jackson-databind-2.7.3.jar:./src/jackson-core-2.7.3.jar:./src/jackson-annotations-2.7.3.jar" AverageDegree ./tweet_input/tweets.txt ./tweet_output/output.txt
 
